3e621d7f064cdf7df6c079b5539cb70beba6490f
luisnasc <luisbrunu@gmail.com>
2021-05-18 01:49:57 -0300

    Ajuste simples

--- java-avancado/src/br/com/dio/lambda/FunctionParameter.java
+++ java-avancado/src/br/com/dio/lambda/FunctionParameter.java
@@ -7,9 +7,7 @@ public class FunctionParameter {
 	public static void main(String[] args) {
 		Function<String, String> getReversedString = str -> new StringBuilder(str).reverse().toString();
 		
-		Function<Double, Double> computeSquareRoot = Math::sqrt;
-		
-		
+		Function<Double, Double> computeSquareRoot = Math::sqrt;		
 		
 		System.out.println(getReversedString.apply("String original"));
 		System.out.println(computeSquareRoot.apply(225.0));
